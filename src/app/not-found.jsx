e345730cd7afcfcaa596fb6f7a907fb1f721d098
MAHEDI HASAN <mahedi5096@gmail.com>
2026-06-16 00:44:54 +0600

    handle notFound

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -11,7 +11,7 @@ export default function NotFound() {
                 
                 <div className="text-center">
                     <h2 className="text-2xl font-semibold">Not Found</h2>
-                    <p className="my-2">Could not find requested resource</p>
+                    <p className="my-2">Could not find requested resource.</p>
                      <Link href="/" rel="noopener noreferrer" className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50"><Button className="bg-lime-400">Back to homepage</Button> </Link>
                 </div>
             </div>
